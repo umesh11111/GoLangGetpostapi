332323dc8db84fe350c0d1a5fe35aa01bdc7759c
umesh11111 <43033939+umesh11111@users.noreply.github.com>
2019-06-21 13:26:18 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, "form.html")
+	case "POST":
+		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+		empid := r.FormValue("Empid")
+		fname := r.FormValue("FirstName")
+		lname := r.FormValue("LastName")
+		address := r.FormValue("address")
+		fmt.Fprintf(w, "empid = %s\n", empid)
+		fmt.Fprintf(w, "fname = %s\n", fname)
+		fmt.Fprintf(w, "lname = %s\n", lname)
+		fmt.Fprintf(w, "Address = %s\n", address)
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+
+	fmt.Printf("Starting server for testing HTTP POST...\n")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal(err)
+	}
+}
